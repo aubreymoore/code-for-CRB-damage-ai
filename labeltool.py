54aff0ebbaa4f43b26ca4bd0d34c0dc9acb2090f
Aubrey Moore <Aubrey Moore>
2024-06-21 20:50:43 +0800

    minors

--- labeltool.py
+++ labeltool.py
@@ -1,6 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 import subprocess
+import sqlite3
+
+print('hi')
+conn = sqlite3.connect('/home/aubrey/Desktop/Guam07-training-set/code/mydb.db')
+cur = conn.cursor()
+cur.execute('''select imagepath from boxes
+            order by conf ASC
+            limit 5''')
+rows = cur.fetchall()
+conn.close()
+print(rows)
+
+for row in rows:
+    print(row[0])
 
 def annotate():
     subprocess.run(

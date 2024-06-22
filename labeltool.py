06bb1abd546fe461cb8ec270b6e6d6ef5c1cafd9
Aubrey Moore <Aubrey Moore>
2024-06-22 15:26:56 +0800

    improved labeltool

--- labeltool.py
+++ labeltool.py
@@ -3,31 +3,85 @@ from tkinter import ttk
 import subprocess
 import sqlite3
 
-print('hi')
 conn = sqlite3.connect('/home/aubrey/Desktop/Guam07-training-set/code/mydb.db')
+conn.row_factory = sqlite3.Row # enables getting results as a list of dicts 
 cur = conn.cursor()
-cur.execute('''select imagepath from boxes
+cur.execute('''select * from boxes
             order by conf ASC
             limit 5''')
 rows = cur.fetchall()
 conn.close()
-print(rows)
 
 for row in rows:
-    print(row[0])
+    print(dict(row))
 
-def annotate():
-    subprocess.run(
+
+
+   
+i = 0
+imax = len(rows) - 1
+proc = None
+
+def annotate(imagepath, proc):
+    # proc = subprocess.run(
+    #     ['python3', 
+    #      '/home/aubrey/labelImg/labelImg.py', 
+    #      imagepath]
+    #     )
+    if proc:
+        proc.terminate()
+    proc = subprocess.Popen(
         ['python3', 
          '/home/aubrey/labelImg/labelImg.py', 
-         '/home/aubrey/Desktop/Guam07-training-set/datasets/test/train/IMG_20221115_112224.jpg']
+         imagepath]
         )
-    
+    return proc
+
+def first():
+    global i
+    i = 0
+    txt_i = ttk.Label(master=window, text=i)
+    txt_i.grid(row=2,column=4)
+    update_and_annotate()
+
+def previous():
+    global i
+    i = max(i-1, 0)
+    txt_i = ttk.Label(master=window, text=i)
+    txt_i.grid(row=2,column=4)
+    update_and_annotate()
+ 
+def next():
+    global i
+    i = min(i+1, imax)
+    txt_i = ttk.Label(master=window, text=i)
+    txt_i.grid(row=2,column=4)
+    update_and_annotate()
+
+def last():
+    global i
+    i = imax
+    update_and_annotate()
+       
+def update_and_annotate(): 
+    global proc   
+    txt_i = ttk.Label(master=window, text=i)
+    txt_i.grid(row=2,column=4)
+    imagepath = rows[i]['imagepath']
+    print('imagepath')
+    proc = annotate(imagepath, proc)
+ 
 window = tk.Tk()
 window.title('labeltool')
-window.geometry('300x150')
+window.geometry('350x175')
 
-button = ttk.Button(master=window, text='Annotate', command=annotate)
-button.place(relx=0.5, rely=0.5, anchor='center')
+btn_first = ttk.Button(master=window, text='first', command=first)
+btn_first.grid(row=2, column=0)
+btn_previous = ttk.Button(master=window, text='previous', command=previous)
+btn_previous.grid(row=2, column=1)
+btn_next = ttk.Button(master=window, text='next', command=next)
+btn_next.grid(row=2, column=2)
+btn_last = ttk.Button(master=window, text='last', command=last)
+btn_last.grid(row=2, column=3)
 
 window.mainloop()

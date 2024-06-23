c2d3a54103bcb1576a84b6516ac98f1963e5684e
Aubrey Moore <Aubrey Moore>
2024-06-24 07:41:32 +0800

    minors

--- labeltool.py
+++ labeltool.py
@@ -2,86 +2,94 @@ import tkinter as tk
 from tkinter import ttk
 import subprocess
 import sqlite3
+from icecream import ic
+
+DATABASE_PATH = '/home/aubrey/Desktop/Guam07-training-set/code/mydb.db'
 
 def get_data():
-    conn = sqlite3.connect('/home/aubrey/Desktop/Guam07-training-set/code/mydb.db')
-    conn.row_factory = sqlite3.Row # enables getting results as a list of dicts 
+    ic()
+    sql = '''
+    select * from boxes
+    order by conf ASC
+    limit 5
+    '''
+    conn = sqlite3.connect(DATABASE_PATH)
+    conn.row_factory = sqlite3.Row # get results as a list of dicts 
     cur = conn.cursor()
-    cur.execute('''select * from boxes
-                order by conf ASC
-                limit 5''')
+    cur.execute(sql)
     rows = cur.fetchall()
     conn.close()
     return rows
 
-def annotate(imagepath, proc):
-    # proc = subprocess.run(
-    #     ['python3', 
-    #      '/home/aubrey/labelImg/labelImg.py', 
-    #      imagepath]
-    #     )
+def annotate():
+    global proc
+    ic()
+    ic(proc)
     if proc:
+        ic('about to terminate proc')
         proc.terminate()
-    proc = subprocess.Popen(
-        ['python3', 
-         '/home/aubrey/labelImg/labelImg.py', 
-         imagepath]
-        )
-    return proc
-
-def first(i):
-    i = 0
-    txt_i = ttk.Label(master=window, text=i)
-    txt_i.grid(row=2,column=4)
-    update_and_annotate(proc)
-    return i
-
-def previous(i):
-    i = max(i-1, 0)
-    txt_i = ttk.Label(master=window, text=i)
-    txt_i.grid(row=2,column=4)
-    update_and_annotate(proc)
-    return i
- 
-def next(i):
-    i = min(i+1, imax)
-    txt_i = ttk.Label(master=window, text=i)
-    txt_i.grid(row=2,column=4)
-    update_and_annotate(proc)
-    return i
-
-def last(i):
-    i = imax
-    update_and_annotate(proc)
-    return i
-       
-def update_and_annotate(proc): 
-    txt_i = ttk.Label(master=window, text=i)
-    txt_i.grid(row=2,column=4)
     imagepath = rows[i]['imagepath']
-    print('imagepath')
-    proc = annotate(imagepath, proc)
-    return proc
+    proc = subprocess.Popen(['python3', '/home/aubrey/labelImg/labelImg.py', imagepath])
+    ic(proc)
 
+def update_and_annotate(i, proc): 
+    ic()
+    annotate()
+
+def first():
+    ic()
+    global i
+    i = 0
+    add_info()
+    
+def previous():
+    ic()
+    global i
+    i = max(i-1, 0)
+    add_info()
+    
+def add_info():
+    txt_i = ttk.Label(master=window, text=f'{i+1} of {imax+1}')
+    txt_i.grid(row=3,column=0)
+    
+    txt_x = ttk.Label(master=window, text=f'x: {round(rows[i]["x"],2)}')
+    txt_x.grid(row=3, column=1)
+    annotate()
+    
+def next():  
+    ic()
+    global i
+    i = min(i+1, imax)
+    add_info()
+    
+def last():
+    ic()
+    global i
+    i = imax
+    add_info()
+       
 # MAIN
 
-rows = get_data()
-i = 0
-imax = len(rows) - 1
-proc = None
-imagepath = None
- 
+# set up GUI
 window = tk.Tk()
 window.title('labeltool')
 window.geometry('350x175')
 
-btn_first = ttk.Button(master=window, text='first', command=first(i))
+btn_first = ttk.Button(master=window, text='first', command=first)
 btn_first.grid(row=2, column=0)
-btn_previous = ttk.Button(master=window, text='previous', command=previous(i))
+btn_previous = ttk.Button(master=window, text='previous', command=previous)
 btn_previous.grid(row=2, column=1)
-btn_next = ttk.Button(master=window, text='next', command=next(i))
+btn_next = ttk.Button(master=window, text='next', command=next)
 btn_next.grid(row=2, column=2)
-btn_last = ttk.Button(master=window, text='last', command=last(i))
+btn_last = ttk.Button(master=window, text='last', command=last)
 btn_last.grid(row=2, column=3)
 
+# Initialize global variables
+rows = get_data()
+i = 0
+imax = len(rows) - 1
+proc = None
+first()
+
+# Start GUI
 window.mainloop()
